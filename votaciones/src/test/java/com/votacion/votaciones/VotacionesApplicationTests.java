c3acbd9587b492c82bcac4521f3e661c1aa17eb9
Talitos7 <carlocaba2004mail.com>
2025-09-10 09:06:18 -0400

    Agragdo de controller, dto y exception

---
+++ votaciones/src/test/java/com/votacion/votaciones/VotacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.votacion.votaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
