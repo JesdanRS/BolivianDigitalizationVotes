51a5e1434a70b81bd9a7c986c094f3da5be03822
Luciana Velasco <luwuvp22@gmail.com>
2025-11-25 20:22:35 -0400

    Auditoria en otros servicios base

--- auditoria_registros-services/src/main/java/com/votaciones/auditoria_registros/config/SecurityConfig.java
+++ auditoria_registros-services/src/main/java/com/votaciones/auditoria_registros/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
                 
                 // Endpoints específicos con roles
                 .requestMatchers(HttpMethod.GET, "/api/auditoria/**").hasAnyRole("USER", "AUDITOR")
-                .requestMatchers(HttpMethod.POST, "/api/auditoria/**").hasRole("AUDITOR")
+                .requestMatchers(HttpMethod.POST, "/api/auditoria/**").hasAnyRole("USER", "ADMIN", "AUDITOR")
                 .requestMatchers(HttpMethod.PUT, "/api/auditoria/**").hasRole("AUDITOR")
                 .requestMatchers(HttpMethod.DELETE, "/api/auditoria/**").hasRole("AUDITOR")
                 
