46f6b0a77fb6c79411412909bb859c820e9fcad4
Luciana Velasco <luwuvp22@gmail.com>
2025-11-28 01:00:20 -0400

    Config y auditoria funcionando en local

--- auditoria_registros-services/src/main/java/com/votaciones/auditoria_registros/config/SecurityConfig.java
+++ auditoria_registros-services/src/main/java/com/votaciones/auditoria_registros/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
                 .requestMatchers("/v3/api-docs/**").permitAll()
                 .requestMatchers("/swagger-ui/**").permitAll()
                 .requestMatchers("/swagger-ui.html").permitAll()
+                .requestMatchers("/webjars/**").permitAll()
                 
                 // Endpoints específicos con roles
                 .requestMatchers(HttpMethod.GET, "/api/auditoria/**").hasAnyRole("USER", "AUDITOR")
